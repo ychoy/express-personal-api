9923701af624a2bbca68e8193d5ce585924b1be7
ychoy <yanyinchoy@gmail.com>
2017-02-13 08:52:52 -0800

    create and delete do not work but got ajax running and results rendered

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,15 @@
-var $campingList;
-var allCamping = [];
+
+//var allCamping = [];
 
 $(document).ready(function(){
 
-  $campingList = $('#campingTarget');
+  $result = $('#campingTarget');
+
   $.ajax({
     method: 'GET',
     url: '/api/camping',
     success: handleSuccess,
-    error: handleError
+  //  error: handleError
   });
 
   $('#newCampingForm').on('submit', function(e) {
@@ -16,15 +17,110 @@ $(document).ready(function(){
     $.ajax({
       method: 'POST',
       url: '/api/camping',
-      data: $(this).serialize(),
+      data: $(this).serializeArray(),
       success: newCampingSuccess,
-      error: newCampingError
+//      error: newCampingError
     });
   });
 
+  $result.on('click', '.deleteBtn', function() {
+    console.log('clicked delete button to', '/api/camping/:id') //+$(this).attr('data-id'));
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/camping/:id',  // +$(this).attr('data-id'),
+      success: deleteCampingSuccess,
+//      error: deleteCampingError
+    });
+  });
+
+});
+
+function handleSuccess(data) {
+    console.log("received data:", data);
+    var campingResults = data;
+    console.log(campingResults);
+
+    if (campingResults.length > 0) {
+
+      // loop over each result
+      campingResults.forEach(function (result, index) {
+            // create object of data
+            var campingData = {
+              title: result.title,
+              park: result.park,
+              images: result.images,
+              description: result.description,
+            };
+
+            var $resultstoDisplay = `
+                    <br>
+                    <div class="row">
+                      <div class="col-xs-4">
+                        <img src="${campingData.images}" class="img-responsive">
+                      </div>
+                      <div class="col-sm-8">
+                        <p> <strong> ${campingData.title} </strong> - ${campingData.park} </p>
+                        <p> ${campingData.description} </p>
+                        <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${campingData._id}>Delete</button>
+                      </div>
+                    </div>
+                    <br>
+                  `;
+
+                  $result.append($resultstoDisplay);
+
+            });
+        } else {  // if there are no results, display this
+            $result.append('<p class="text-center">No results</p>');
+        }
+      }
+
 /*
+function handleSuccess(json) {
+  json(allCamping);
+
+function handleError(e) {
+  console.log('uh oh');
+  $('#campingTarget').text('Failed to load camping, is the server working?');
+}
+
+function newCampingSuccess(json) {
+  $('#newCampingForm input').val('');
+  allCamping.push(json);
+
+}
+
+
+function newCampingError() {
+  console.log('newCamping error!');
+}
+
+
+function deleteCampingSuccess(json) {
+  var camping = json;
+  console.log(json);
+  var campingid = camping._id;
+  console.log('delete camping', campingid);
+  // find the camping with the correct ID and remove it from our allCamping array
+  for(var index = 0; index < allCamping.length; index++) {
+    if(allCamping[index]._id === campingid) {
+      allCamping.splice(index, 1);
+      break;  // we found our camping - no reason to keep searching (this is why we didn't use forEach)
+    }
+  };
+
+  json(allCamping);
+};
+*/
+/*
+function deleteCampingError() {
+  console.log('deleteCamping error!');
+};
+*/
+
+/* Add later
     // for update: submit event on `.update` form
-    $campingList.on('submit', '.update', function (event) {
+    $result.on('submit', '.update', function (event) {
       event.preventDefault();
       // find the camping id (stored in HTML as `data-id`)
       var campingId = $(this).closest('.camping').attr('data-id');
@@ -49,79 +145,3 @@ $(document).ready(function(){
     })
 
   */
-
-  $campingList.on('click', '.deleteBtn', function() {
-    console.log('clicked delete button to', '/api/camping/'+$(this).attr('data-id'));
-    $.ajax({
-      method: 'DELETE',
-      url: '/api/camping/'+$(this).attr('data-id'),
-      success: deleteCampingSuccess,
-      error: deleteCampingError
-    });
-  });
-
-});
-
-function getCampingHtml(camping) {
-  return `<hr>
-          <p>
-            <b>${camping.title}</b>
-            - ${camping.park}
-            <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${camping._id}>Delete</button>
-          </p>`;
-}
-
-function getAllCampingHtml(camping) {
-  return camping.map(getCampingHtml).join("");
-}
-
-// helper function to render all posts to view
-// note: we empty and re-render the collection each time our post data changes
-function render () {
-  // empty existing posts from view
-  $campingList.empty();
-
-  // pass `allCamping` into the template function
-  var campingHtml = getAllCampingHtml(allCamping);
-
-  // append html to the view
-  $campingList.append(CampingHtml);
-};
-
-function handleSuccess(json) {
-  allCamping = json;
-  render();
-}
-
-function handleError(e) {
-  console.log('uh oh');
-  $('#campingTarget').text('Failed to load camping, is the server working?');
-}
-
-function newCampingSuccess(json) {
-  $('#newCampingForm input').val('');
-  allCamping.push(json);
-  render();
-}
-
-function newCampingError() {
-  console.log('new camping error!');
-}
-
-function deleteCampingSuccess(json) {
-  var camping = json;
-  console.log(json);
-  var campingId = camping._id;
-  console.log('delete camping', campingId);
-  for(var index = 0; index < allCamping.length; index++) {
-    if(allCamping[index]._id === campingId) {
-      allCamping.splice(index, 1);
-      break;
-    }
-  }
-  render();
-}
-
-function deleteCampingError() {
-  console.log('deletecamping error!');
-}
